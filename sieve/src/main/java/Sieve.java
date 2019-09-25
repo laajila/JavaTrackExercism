92b78fb1f1c333de74e65fa6c41f7db0c867da22
BENABACHIR Issam <issam.benabachir@gmail.com>
2019-09-25 21:40:01 +0200

    sieve

---
+++ sieve/src/main/java/Sieve.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Sieve {
+    int maxPrime;
+    private Set<Integer> notprimes= new TreeSet<>();
+    Sieve(int maxPrime) {
+        this.maxPrime=maxPrime;
+        for (int i = 2; i <= maxPrime; i++) {
+            int notPrime;
+            notPrime = 0;
+            for (int j = 2; notPrime < maxPrime; j++) {
+                notPrime=j*i;
+                notprimes.add(notPrime);
+            }
+        }
+    }
+
+    List<Integer> getPrimes() {
+        List<Integer> primes=new ArrayList<Integer>();
+        for (int i = 2; i <=this.maxPrime ; i++) {
+            if(!notprimes.contains(i)) primes.add(i);
+        }
+        return primes;
+    }
+
+
+}
