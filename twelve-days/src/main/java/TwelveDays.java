9c24d5b18b03f8b9cb2250014678ff74fe64fda4
BENABACHIR Issam <issam.benabachir@gmail.com>
2019-09-27 13:01:27 +0200

    verses and sing

--- twelve-days/src/main/java/TwelveDays.java
+++ twelve-days/src/main/java/TwelveDays.java
@@ -1,45 +1,49 @@
 class TwelveDays {
-    String startOfVerse=" day of Christmas my true love gave to me:";
-    String [] elementsOfVerses={"a Partridge in a Pear Tree",
-                                "two Turtle Doves",
-                                "three French Hens",
-                                "four Calling Birds",
-                                "five Gold Rings",
-                                "six Geese-a-Laying",
-                                "seven Swans-a-Swimming",
-                                "eight Maids-a-Milking",
-                                "nine Ladies Dancing",
-                                "ten Lords-a-Leaping",
-                                "eleven Pipers Piping",
-                                "twelve Drummers Drumming"};
+    String startOfVerse = " day of Christmas my true love gave to me: ";
+    String[] elementsOfVerses = {"a Partridge in a Pear Tree",
+            "two Turtle Doves",
+            "three French Hens",
+            "four Calling Birds",
+            "five Gold Rings",
+            "six Geese-a-Laying",
+            "seven Swans-a-Swimming",
+            "eight Maids-a-Milking",
+            "nine Ladies Dancing",
+            "ten Lords-a-Leaping",
+            "eleven Pipers Piping",
+            "twelve Drummers Drumming"};
 
-    String [] theDay={"On the first","On the second","On the third","On the fourth","On the fifth","On the sixth",
-            "On the seventh","On the eighth","On the ninth","On the tenth","On the eleventh","On the twelfth"};
+    String[] theDay = {"On the first", "On the second", "On the third", "On the fourth", "On the fifth", "On the sixth",
+            "On the seventh", "On the eighth", "On the ninth", "On the tenth", "On the eleventh", "On the twelfth"};
 
-    TwelveDays(){
+    TwelveDays() {
 
 
     }
+
     String verse(int verseNumber) {
-        StringBuilder verse=new StringBuilder();
-        verse.append(this.theDay[verseNumber-1]).append(this.startOfVerse);
-        if(verseNumber==1) {
-            verse.append(" ").append(elementsOfVerses[0]).append(".").append("\n");
+        StringBuilder verse = new StringBuilder();
+        verse.append(this.theDay[verseNumber - 1]).append(this.startOfVerse);
+
+        for (int i = verseNumber - 1; i > 0; i--) {
+            verse.append(elementsOfVerses[i]).append(", ");
         }
-        else {
-            for (int i = verseNumber - 1; i > 0; i--) {
-                verse.append(" ").append(elementsOfVerses[i]).append(",");
-            }
-            verse.append(" and ").append(elementsOfVerses[0]).append(".").append("\n");
-        }
+        if (verseNumber!=1) verse.append("and ");
+        verse.append(elementsOfVerses[0]).append(".").append("\n");
+
         return verse.toString();
     }
 
     String verses(int startVerse, int endVerse) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        StringBuilder part= new StringBuilder();
+        for (int i = startVerse ; i <=endVerse; i++) {
+            part.append(verse(i)).append("\n");
+        }
+        part.setLength(part.length() - 1);
+        return part.toString();
     }
-    
+
     String sing() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return verses(1,12);
     }
 }
