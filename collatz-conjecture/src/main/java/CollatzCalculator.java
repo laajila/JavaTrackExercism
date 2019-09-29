d9e949a4c7fa4622b6d80c282d328761b3c62a04
Issam Benabachir <issam.benabachir@gmail.com>
2019-09-29 22:15:19 +0200

    added exceptions for 0 and negative numbuers

--- collatz-conjecture/src/main/java/CollatzCalculator.java
+++ collatz-conjecture/src/main/java/CollatzCalculator.java
@@ -1,6 +1,7 @@
 class CollatzCalculator {
 
     int computeStepCount(int start) {
+        if (start<=0) throw new IllegalArgumentException("Only natural numbers are allowed");
         int stepCount=0;
         while (start!=1){
             if (start%2==0) start= start/2;
