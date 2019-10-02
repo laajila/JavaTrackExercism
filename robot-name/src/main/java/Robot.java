cad323d40a56ae50c5f2a1f30150e5e302d02b2b
Issam Benabachir <issam.benabachir@gmail.com>
2019-10-02 22:44:33 +0200

    solution robot

--- robot-name/src/main/java/Robot.java
+++ robot-name/src/main/java/Robot.java
@@ -1,10 +1,41 @@
-/*
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+class Robot{
+    private String name;
+    private char[] digits="0123456789".toCharArray();
+    private char[] Letters="ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+    private static List<String> robotRegistry=new ArrayList<String>();
+    private Random generator;
+    Robot(){
+        this.generator=new Random();
+    }
 
-Please remove this comment when submitting your solution.
+    String getName(){
+        this.name=generateName();
+        if (robotRegistry.contains(this.name)){
+            this.name=generateName();
+            return this.name;
+        }
+        robotRegistry.add(this.name);
+        return this.name;
+    }
 
-*/
+    private String generateName(){
+        StringBuilder name=new StringBuilder();
+        for (int i = 0; i <2 ; i++) {
+            name.append(Letters[generator.nextInt(26)]);
+        }
+        for (int i = 0; i <3 ; i++) {
+            name.append(digits[generator.nextInt(10)]);
+        }
+        return name.toString();
+    }
+
+    void reset(){
+        robotRegistry.remove(this.name);
+        getName();
+        robotRegistry.add(this.name);
+    }
+}
