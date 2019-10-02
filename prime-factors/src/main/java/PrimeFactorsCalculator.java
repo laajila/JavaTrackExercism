8dcf222863dc07ba0b1766084cabbc5c5d56b4be
Issam Benabachir <issam.benabachir@gmail.com>
2019-10-02 23:30:45 +0200

    primeFactorsCalculator solution

--- prime-factors/src/main/java/PrimeFactorsCalculator.java
+++ prime-factors/src/main/java/PrimeFactorsCalculator.java
@@ -1,4 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 class PrimeFactorsCalculator {
 
 
+    List<Long> calculatePrimeFactorsOf(Long num) {
+        List<Long> factors = new ArrayList<>();
+        long i = 2L;
+        while ( num!=1L) {
+            if (isPrime(i) && num % i == 0 ) {
+                factors.add(i);
+                num=num/i;
+            }else{
+                i++;
+            }
+        }
+
+        return factors;
+    }
+
+    boolean isPrime(long l) {
+        if (l <= 1) return false;
+        for (int i = 2; i < Math.sqrt(l); i++) {
+            if (l % i == 0) return false;
+        }
+        return true;
+    }
 }
