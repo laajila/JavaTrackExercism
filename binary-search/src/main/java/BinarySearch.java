a465ecc61b8a2dcc74bce66acd856fd8ea72f4f7
Issam Benabachir <issam.benabachir@gmail.com>
2019-10-12 12:38:22 +0200

    binary search

---
+++ binary-search/src/main/java/BinarySearch.java
@@ -0,0 +1,29 @@
+import java.util.List;
+
+class BinarySearch {
+    List<Integer> sortedList;
+
+    BinarySearch(List<Integer> sortedList) {
+        this.sortedList = sortedList;
+    }
+
+    int indexOf(int intTobeFound) throws ValueNotFoundException {
+        int index = -1;
+        int start = 0;
+        int end = sortedList.size() - 1;
+
+        while (start <= end) {
+            int middle = (end + start) / 2;
+            if (intTobeFound > sortedList.get(middle)) {
+                start = middle + 1;
+
+
+            } else if (intTobeFound < sortedList.get(middle)) {
+                end = middle - 1;
+            } else if (intTobeFound == sortedList.get(middle)) {
+                return middle;
+            }
+        }
+        throw new ValueNotFoundException("Value not in array");
+    }
+}
