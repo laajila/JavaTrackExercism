32865f6cf0b46fe4b541cb42e3851c7b6cb0eab3
Issam Benabachir <issam.benabachir@gmail.com>
2019-10-13 16:57:12 +0200

    cleaned up bowlin

--- bowling/src/main/java/BowlingGame.java
+++ bowling/src/main/java/BowlingGame.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 class BowlingGame {
-    private List<List<Integer>> rolls;
-    int frame=0;
-    int frameCounter=0;
+    private List<Integer> rolls;
+    int frame = 0;
+    String typeFrame;//open, spare,strike
+    int frameCounter = 0;
 
 
     BowlingGame() {
@@ -14,35 +15,16 @@ class BowlingGame {
     }
 
     void roll(int pins) {
-        frameCounter++;
 
-        if (pins == 10 && frameCounter==1) {
-            score += 10 + rolls.get(i + 1) + rolls.get(i + 2);
-            i++;
-        }else if (rolls.get(i) + rolls.get(i + 1) == 10 ){
-            score += rolls.get(i) + rolls.get(i + 1) + rolls.get(i + 2);
-            i=i+2;
-        }else{
-            score +=rolls.get(i) + rolls.get(i + 1);
-            i=i+2;
-        }
-        rolls.add(pins);
     }
 
     int score() {
         int score = 0;
-        int i = 0;
-        while (i < rolls.size()-2) {
 
-        }
 
-        if(rolls.size()==20) score +=rolls.get( rolls.size()-2) + rolls.get( rolls.size()-1);
 
         return score;
     }
-
-
-
 
 
 }
